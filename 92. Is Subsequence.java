95cbe2f258a773ff0dbaf9c680dfa2ca2e0e8465
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2023-12-13 15:30:11 +0530

    Create 92. Is Subsequence.java

---
+++ 92. Is Subsequence.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if(s.length() == 0){
+            return true;
+        }
+        int i = 0, j = 0;
+        while(j < t.length()){
+            if(i == s.length()) return true;
+            if(s.charAt(i) == t.charAt(j)){
+                i++;
+                j++;
+            } else {
+                j++;
+            }
+        }
+        return i == s.length();
+    }
+}
