db35c40ad2d7013ed80bdf4b835e5375a0546053
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2023-12-15 04:06:20 +0530

    Update 1679. Max Number of K-Sum Pairs.java

--- 1679. Max Number of K-Sum Pairs.java
+++ 1679. Max Number of K-Sum Pairs.java
@@ -1,3 +1,5 @@
+// nLog(n) time and contant space
+
 class Solution {
     public int maxOperations(int[] nums, int k) {
         int n = nums.length, count = 0;
@@ -13,6 +15,26 @@ class Solution {
                 count++;
                 i++;
                 j--;
+            }
+        }
+        return count;
+    }
+}
+
+
+// Using Hashmap O(n) time and extra space
+
+class Solution {
+    public int maxOperations(int[] nums, int k) {
+        int n = nums.length, count = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < n; i++){
+            int key = k - nums[i];
+            if(map.containsKey(key) && map.get(key) > 0){
+                count++;
+                map.put(key, map.get(key) - 1);
+            } else {
+                map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
             }
         }
         return count;
