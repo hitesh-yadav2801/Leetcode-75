cf0d92d45dd3036d94f704f9550c905776d2847c
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2023-12-17 00:47:04 +0530

    Update 724. Find Pivot Index.java

--- 724. Find Pivot Index.java
+++ 724. Find Pivot Index.java
@@ -1,3 +1,29 @@
+// Using extra space
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int n = nums.length;
+        int[] prefix = new int[n];
+        prefix[0] = nums[0];
+        for(int i = 1; i < n; i++){
+            prefix[i] = prefix[i - 1] + nums[i];
+        }
+        int pre = 0;
+        int max = prefix[n - 1];
+        if(max == prefix[0]){
+            return 0;
+        }
+        for(int i = 1; i < n; i++){
+            if(max - prefix[i] == prefix[i - 1]){
+                return i;
+            }
+
+        }
+        return -1;
+    }
+}
+
+// Without any extra space
+
 class Solution {
     public int pivotIndex(int[] nums) {
         int n = nums.length;
